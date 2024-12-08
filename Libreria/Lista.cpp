1501659df7a91feda40e5042602b38b5bcbca3c6
hanumonke <hanumansanchez@gmail.com>
2024-12-08 00:24:44 -0400

    fix: added logic of new voting process

--- Libreria/Lista.cpp
+++ Libreria/Lista.cpp
@@ -355,6 +355,13 @@ nodoPrioridad<Tipo>* Lista<Tipo>::ObtPrimeroPrioridad() {
 };
 
 template <class Tipo>
+nodoPrioridad<Tipo>* Lista<Tipo>::SacarTope() {
+    nodoPrioridad<Tipo> *tope = ObtPrimeroPrioridad(); 
+    PrimeroPrioridad = tope->prox; 
+    return tope; 
+}
+
+template <class Tipo>
 nodoPrioridad<Tipo>* Lista<Tipo>::ObtProxPrioridad(ApuntadorPrioridad p) {
     return p->prox;
 };
--- Votantes.h
+++ Votantes.h
@@ -26,6 +26,7 @@ class Votantes {
     //Votacion
     void ListarCandidatos(Lista<Candidato>& candidatos, bool mostrarVotos);
     bool ProcesarVotantes(Lista<Candidato>& candidatos); 
+    bool procesarVoto(Lista<Candidato>& candidatos, int posicion); 
 
     //Mostrar los electores registrados
     void MostrarElectores();
